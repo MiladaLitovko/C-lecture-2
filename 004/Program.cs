9aa0fd59865d4a81aa7c5926d21806d7092808c0
litovkomilada <litovkomiada@yandex.ru>
2022-06-25 14:49:13 +0300

    Complete 004

---
+++ 004/Program.cs
@@ -0,0 +1,50 @@
+﻿void FiLLArray(int[] collection)
+{
+    int lenght = collection.Length;
+    int index = 0;
+    while(index < lenght)
+    {
+        collection[index] = new Random().Next(1, 10);
+        index++;
+    }
+}
+
+
+void PrintArray(int[] col)
+{
+    int count = col.Length;
+    int position = 0;
+    while(position < count)
+    {
+        Console.WriteLine(col[position]);
+        position++;
+    }
+}
+
+
+
+int Indexof(int[] collection, in find)
+{
+    int count = collection.Length;
+    int index = 0;
+
+    while (index < count)
+    {
+        if(collection[index] == find)
+        {
+            position = index;
+        }
+        index++;
+    }
+    return position;
+}
+
+
+int[] array = new int[10];
+
+FiLLArray(array);
+PrintArray(array);
+Console.WriteLine();
+
+int pos = Indexof[array, 4];
+Console.WriteLine(pos);
