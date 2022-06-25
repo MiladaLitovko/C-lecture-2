c3b02cf5f5e6f5fdaa25600e4a9d65ca47942b4f
litovkomilada <litovkomiada@yandex.ru>
2022-06-25 14:35:07 +0300

    Complete 003

---
+++ 003_Massive+/Program.cs
@@ -0,0 +1,16 @@
+﻿int[] array = {1, 2, 33, 92, 65, 6, 67, 8, 92};
+
+int n = array.Length;
+int find = 92;
+
+int index = 0;
+
+while (index < n)
+{
+    if(array[index] == find)
+    {
+        Console.WriteLine(index);
+        break;
+    }
+    index++;
+}
