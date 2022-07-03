5ad1f19dadce6e80779e7850225d0ca2a1ac869f
litovkomilada <litovkomiada@yandex.ru>
2022-07-03 16:02:46 +0300

    Methods

--- 005/Program.cs
+++ 005/Program.cs
@@ -62,7 +62,7 @@ for(int i = 2; i <= 10; i++)
 {
     for(int j = 2; j <= 10; j++)
     {
-        Console.WriteLine($"{i} x {j} = {i * j}");
+        //Console.WriteLine($"{i} x {j} = {i * j}");
     }
     //Console.WriteLine();
 }
@@ -96,7 +96,46 @@ string Replace(string text, char oldValue, char newValue)
 }
 
 string newText = Replace(text, ' ', '/');
-Console.WriteLine(newText);
-Console.WriteLine();
+//Console.WriteLine(newText);
+//Console.WriteLine();
 newText = Replace(newText, 'к', 'К');
-Console.WriteLine(newText);
+//Console.WriteLine(newText);
+
+// ЗАДАЧА 2
+
+int[] arr = {1, 5, 4, 3, 2, 6, 7, 1, 1};
+
+void PrintArray(int[] array)
+{
+    int count = array.Length;
+
+    for (int i = 0; i < count; i++)
+    {
+        Console.Write($"{array[i]} ");
+    }
+    Console.WriteLine();
+}
+
+void SelectionSort(int[] array)
+{
+    
+    for (int i = 0; i < array.Length - 1; i++)
+    {
+        int minPosition = i;
+
+        for (int j = i + 1; j < array.Length; j++)
+        {
+            if(array[j] < array[minPosition]) minPosition = j;
+        }
+
+
+        int temporary = array[i];
+        array[i] = array[minPosition];
+        array[minPosition] = temporary;
+    }
+}
+
+PrintArray(arr);
+SelectionSort(arr);
+
+PrintArray(arr);
