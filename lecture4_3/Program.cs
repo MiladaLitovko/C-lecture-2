e631e005a89614b59acc44c9e8204f42115d6a11
litovkomilada <litovkomiada@yandex.ru>
2022-07-12 14:12:03 +0300

    Complete 4_3

--- lecture4_3/Program.cs
+++ lecture4_3/Program.cs
@@ -7,3 +7,16 @@ double Factorial (int n)
 }
 
 Console.WriteLine(Factorial(5));
+
+
+// ФИБОНАЧИ
+double Fibonacci(int n)
+{
+    if(n == 1 || n == 2) return 1;
+    else return Fibonacci(n-1) + Fibonacci(n-2);
+}
+
+for (int i = 1; i < 40; i++)
+{
+    Console.WriteLine($"f({i}) = {Fibonacci(i)}");
+}
