310215320e13ec860bc31be32b73dd49c1ee2e97
litovkomilada <litovkomiada@yandex.ru>
2022-07-12 13:13:15 +0300

    Complete 4_2

---
+++ lecture4_2/Program.cs
@@ -0,0 +1,30 @@
+﻿int[,] pic = new int[,]
+
+void PrintImage(int[,] image)
+{
+    for(int i = 0; i < image.GetLength(0); i++)
+    {
+        for(int j = 0; j < image.GetLength(1); j++)
+        {
+           // Console.Write($"{image[i, j]}");
+           if(image[i,j] == 0) Console.Write($"");
+           else Console.Write($"+");
+        }
+        Console.WriteLine();
+    }
+}
+
+//Заполнить рисунок
+PrintImage(pic);
+
+void FillImage(int row, int col)
+{
+    if (pic[row, col] == 0)
+    {
+        pic[row, col] = 1;
+        FillImage(row - 1, col);
+        FillImage(row, col - 1);
+        FillImage(row + 1, col);
+        FillImage(row, col + 1);
+    }
+}
